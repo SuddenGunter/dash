085c68ac96cb7c04d6291ed1fd66ebf3688a1f83
SuddenGunter <kolomytsev1996@gmail.com>
2024-09-28 22:12:53 +0100

    configurable timer

--- lib/dash_web/router.ex
+++ lib/dash_web/router.ex
@@ -20,7 +20,6 @@ defmodule DashWeb.Router do
     get "/", HomeController, :home
     post "/timer", HomeController, :timer
     live "/timer/:id", TimerLive, :timer_live
-    live "/editlive", TimerEditLive, :timer_edit_live
   end
 
   # Other scopes may use custom stacks.
