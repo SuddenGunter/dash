12a8b9a35f43a2173dd54acc1e76fe3867baaa5e
SuddenGunter <kolomytsev1996@gmail.com>
2024-10-21 23:52:14 +0100

    add logs

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,7 +1,9 @@
 let NotificationsAPI = {};
 
 NotificationsAPI.RequestPermission = function () {
+    console.log("NotificationsAPI: requesting permissions");
     if (Notification.permission === "granted" || Notification.permission === "denied") {
+        console.log("NotificationsAPI: permissions already granted or denied");
         return;
     }
 
@@ -10,15 +12,19 @@ NotificationsAPI.RequestPermission = function () {
 
 NotificationsAPI.Enabled = function () {
     if (Notification.permission === "granted") {
+        console.log("NotificationsAPI: Enabled: permissions granted");
         return true;
     }
 
+    console.log("NotificationsAPI: Enabled: permissions not granted");
     return false;
 }
 
 
 NotificationsAPI.Send = function (Msg) {
+    console.log("NotificationsAPI: Send");
     if (NotificationsAPI.Enabled()) {
+        console.log("NotificationsAPI: Send: sending notification");
         const notification = new Notification(Msg);
     }
 }
