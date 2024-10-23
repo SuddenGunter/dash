c0095dda6fe7b53dee2defee963036eb72126479
SuddenGunter <kolomytsev1996@gmail.com>
2024-10-24 00:49:32 +0100

    rm metadata from prod

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,7 @@ config :swoosh, local: false
 
 # Do not print debug messages in production
 config :logger,
-  level: :info,
-  metadata: [:topic]
+  level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
